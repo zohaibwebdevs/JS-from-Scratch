e81afbae985763086bcba093006948c53ed6a423
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2024-08-20 05:50:55 +0000

    config dione

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,10 @@
+var employName = "Zohaib";
+let post = "manager";
+const employId = 12345;
+
+console.table([employName, post, employId]);
+
+// we can change the value of var 
+// let can also be change
+// const value can not change 
+// var is global scope and let is block scope
