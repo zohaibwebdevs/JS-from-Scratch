507c804884fcbeee7dce1bf0381ee80da92cb3f5
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2024-08-22 07:22:24 +0000

    Data Types and conversion

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,27 @@
+// JavaScript Data Types
+
+let num = 10; // Number
+
+let name = "Zohaib"; // String
+
+let isGraduate = true; // Boolean
+
+let num1 = BigInt ("1234567890000000"); // BigInt
+
+let salary; //Undefined
+
+let defects = null; // Null
+
+// Conversion of Data Types
+
+let str1 = "33";
+
+let num3 = Number(str1);
+
+// console.log(typeof(num3));
+
+let num4 = 100;
+
+let str2 = String(num4);
+
+console.log(typeof(str2));
