c0f6b971f9b0205db7df13f856dc417e0fe94829
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2024-09-01 05:49:36 +0000

    string and its methods

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,13 @@
+// string and its methods
+
+let str = "Apple"
+console.log(str);
+
+let newStr = new String("Mango")
+console.log(newStr);
+
+console.log(str.length);
+
+console.log(str.indexOf("l"));
+
+console.log(newStr.charAt(2));
