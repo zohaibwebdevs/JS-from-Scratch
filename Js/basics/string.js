7faa07d88bb04fd0b0e83a3d2f19d7faa2754f0e
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2024-09-02 18:10:52 +0000

    Js basics

--- Js/basics/string.js
+++ Js/basics/string.js
@@ -3,7 +3,7 @@
 let str = "Apple"
 console.log(str);
 
-let newStr = new String("Mango")
+let newStr = new String("MANGO")
 console.log(newStr);
 
 console.log(str.length);
@@ -11,3 +11,8 @@ console.log(str.length);
 console.log(str.indexOf("l"));
 
 console.log(newStr.charAt(2));
+
+console.log(str.toUpperCase());
+
+console.log(newStr.toLowerCase());
+
