1d09dfb358c345dc230d9a5f76c9918b642111d1
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2024-09-11 11:45:19 +0000

    Date in js

--- Js/basics/date.js
+++ Js/basics/date.js
@@ -1,4 +1,20 @@
 let myDate = new Date()
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+
+let newDate = new Date(2024, 0, 15)
+let newDate1 = new Date("2024/1/15")
+let newDate2 = new Date(2024, 1, 20, 5, 30)
+
+// console.log(newDate.toLocaleString());
+// console.log(newDate1.toLocaleString());
+// console.log(newDate.toLocaleString());
+
+let timeStamp = Date.now();
+console.log(timeStamp);
+
+console.log(newDate.getTime());
+
+console.log(Date.now()/1000);
+console.log(Math.floor(Date.now()/1000));
