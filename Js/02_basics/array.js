3a1aafc5358573059ec608d641992ce36e35def8
Muhammad Zohaib <127778092+zohaibwebdevs@users.noreply.github.com>
2024-10-02 18:04:14 +0000

    Js array and its method

--- Js/02_basics/array.js
+++ Js/02_basics/array.js
@@ -1,2 +1,18 @@
 let myArr = [0, 1, 2, 3, 4, 5]
 
+myArr.push(6, 7, 8);
+
+// myArr.length = 10;
+
+// console.log(myArr);
+
+let cars = ["BMW", "Suzuki", "Honda"]
+
+cars[5] = "Hilex";
+
+cars.forEach = ((item, index) => {
+    console.log(`${index}: ${item}`)
+})
+
+// console.log(cars);
+
